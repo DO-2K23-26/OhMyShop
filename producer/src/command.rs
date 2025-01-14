27aa1c48da025fe3fa5be60f4b17567325d3c68b
Dorian TETU <doriantetu34@gmail.com>
2025-01-14 15:39:43 +0100

    feat: integrate rdkafka for command processing and update dependencies

--- producer/src/command.rs
+++ producer/src/command.rs
@@ -1,13 +1,17 @@
 use async_trait::async_trait;
-use common::{client::Client, command::CommandInterface};
 use sqlx::PgPool;
 use chrono::DateTime;
 use rand::Rng;
+use rdkafka::producer::{BaseProducer, BaseRecord};
 
-use crate::product::Product;
+use common::client::Client;
+use common::product::Product;
+use common::command::CommandInterface;
+
 
 #[derive(Debug)]
 #[allow(dead_code)]
+#[derive(serde::Serialize, serde::Deserialize)]
 pub struct MyCommand {
     id: i32,
     client_id: i32,
@@ -26,7 +30,7 @@ impl CommandInterface for MyCommand {
         }
     }
 
-    async fn insert_into_db(&self, pool: &PgPool) -> Result<(), sqlx::Error> {
+    async fn process_command(&self, pool: &PgPool, producer: &BaseProducer, _topic_name: &str) -> Result<(), sqlx::Error> {
         //retrieve random products from database
         let product_limit: i64 = rand::thread_rng().gen_range(1..=10);
 
@@ -38,6 +42,17 @@ impl CommandInterface for MyCommand {
         .fetch_one(pool)
         .await?;
 
+        let client_payload = serde_json::to_string(&client_object).unwrap();
+        if let Err(e) = producer.send(
+            BaseRecord::to("Client")
+                .payload(&client_payload)
+                .key(&client_object.id.to_string()),
+        ) {
+            eprintln!("Failed to send message: {:?}", e);
+        } else {
+            println!("Message produced: {}", client_payload);
+        }
+
         let products = sqlx::query_as!(
             Product,
             "SELECT * FROM Product ORDER BY RANDOM() LIMIT $1",
@@ -46,23 +61,47 @@ impl CommandInterface for MyCommand {
         .fetch_all(pool)
         .await?;
 
-        let command_id = sqlx::query!(
-            "INSERT INTO Command (clientId, date) VALUES ($1, $2) RETURNING id",
+        let command = sqlx::query!(
+            r#"
+            INSERT INTO Command (clientId, date) 
+            VALUES ($1, $2) 
+            RETURNING id, clientId AS "client_id", date
+            "#,
             client_object.id,
             self.date.naive_utc().date()
         )
         .fetch_one(pool)
-        .await?
-        .id;
+        .await?;
+
+        let command_payload = serde_json::to_string(&self).unwrap();
+        if let Err(e) = producer.send(
+            BaseRecord::to("Command")
+                .payload(&command_payload)
+                .key(&command.id.to_string()),
+        ) {
+            eprintln!("Failed to send message: {:?}", e);
+        } else {
+            println!("Message produced: {}", command_payload);
+        }
 
         for product in products {
             sqlx::query!(
                 "INSERT INTO CommandProduct (commandId, productId) VALUES ($1, $2)",
-                command_id,
+                command.id,
                 product.id
             )
             .execute(pool)
             .await?;
+            let product_payload = serde_json::to_string(&product).unwrap();
+            if let Err(e) = producer.send(
+                BaseRecord::to("Product")
+                    .payload(&product_payload)
+                    .key(&product.id.to_string()),
+            ) {
+                eprintln!("Failed to send message: {:?}", e);
+            } else {
+                println!("Message produced: {}", product_payload);
+            }    
         }
 
         Ok(())
